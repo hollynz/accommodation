09f9e401cea96c067da192d07ebe2b8aae068427
Holly Johnson <hollyjnz@gmail.com>
2018-12-09 09:56:06 +1300

    summary screen

--- js/main.js
+++ js/main.js
@@ -203,7 +203,6 @@ function backToForm() {
 };
 
 function filterByUserInput () {
-<<<<<<< HEAD
     var filteredAccommodation;
     var days = calcDays();
     var guests = noOfGuestsEl.val();
@@ -213,16 +212,6 @@ function filterByUserInput () {
     });
     return filteredAccommodation;
 };
-=======
-    var days = calcDays();
-    var guests = noOfGuestsEl.val();
-    var geosearchGlassInputEl = $('.geosearch .glass:last-child');
-    // console.log(typeof geosearchGlassInputEl);
-    console.log(geosearchGlassInputEl);
-    var location = geosearchGlassInputEl.html();
-    console.log('days: ' + days + ' ' + 'guests: ' + guests + ' ' + 'location: ' + location);
-}
->>>>>>> ddb8875cd1c224e400f451d78386016c3a737d51
 
 function calcDays () {
     var dates = [dateFromEl.val().split('/'), dateToEl.val().split('/')];
@@ -230,7 +219,6 @@ function calcDays () {
     var t2 = new Date((dates[1])[2], (dates[1])[0], (dates[1])[1]).getTime();
     var difference = t2 - t1;
     var days = Math.floor(difference/(1000*60*60*24));
-<<<<<<< HEAD
     return days;
 };
 
@@ -245,10 +233,5 @@ function setUpGrid(filteredAccommodation) {
 function getAccommodationSummaryHTML(option) {
     return `<div><img src="${option.imgSrc}"></div>`;
 };
-=======
-    console.log(days);
-    return days;
-}
->>>>>>> ddb8875cd1c224e400f451d78386016c3a737d51
 
 init();
